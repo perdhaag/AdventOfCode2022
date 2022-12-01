983aeca01a636c999758707923b0dd74547693d0
Torgeir Sandnes Laurvik <torgeir.laurvik@gmail.com>
2022-12-02 00:46:44 +0100

    feat: added autodownload of input

--- nodeAoC/src/day1/index.ts
+++ nodeAoC/src/day1/index.ts
@@ -3,7 +3,6 @@ import {
   flow,
   split,
   sum,
-  toString,
   map,
   parseInt,
   head,
@@ -11,15 +10,17 @@ import {
   reverse,
 } from "lodash/fp";
 import { sortBy } from "lodash";
-import * as path from "path";
-
-const file = fs.readFileSync(path.resolve(__dirname, "./input.txt")).toString();
+import { getInput } from "../helpers/retrieveInput";
 
 const sumBlock = flow(split("\n"), map(parseInt(10)), sum);
-
 const totalsPerBlock = flow(split("\n\n"), map(sumBlock), sortBy, reverse);
 
 const part1 = flow(totalsPerBlock, head);
 const part2 = flow(totalsPerBlock, take(3), sum);
 
-console.log(part1(file), part2(file));
+const solve = async () => {
+  const file = await getInput(1);
+  console.log(part1(file), part2(file));
+};
+
+solve();
---
+++ nodeAoC/src/helpers/retrieveInput.ts
@@ -0,0 +1,34 @@
+import * as fs from "fs";
+import path from "path";
+import axios from "axios";
+import { trim } from "lodash";
+require("dotenv").config();
+
+const fetchInput = async (dayNumber: number) => {
+  const content = await axios.get(
+    `https://adventofcode.com/2022/day/${dayNumber}/input`,
+    {
+      headers: {
+        "user-agent": process.env.USER_AGENT,
+        cookie: `session=${process.env.SESSION}` ?? "",
+      },
+    }
+  );
+  const data = trim(content.data);
+  //console.log(content.data.toString());
+  fs.writeFileSync(
+    path.resolve(__dirname, `../day${dayNumber}/input.txt`),
+    data
+  );
+  return data;
+};
+
+export const getInput = async (dayNumber: number) => {
+  try {
+    return fs
+      .readFileSync(path.resolve(__dirname, `../day${dayNumber}/input.txt`))
+      .toString();
+  } catch {
+    return fetchInput(dayNumber);
+  }
+};
