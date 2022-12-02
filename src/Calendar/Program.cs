517cfa70340236be2cf07cd34d7b5e0da776f441
NCG\per.haagensen <per.haagensen@novanet.no>
2022-12-02 12:06:48 +0100

    some changes

--- src/Calendar/Program.cs
+++ src/Calendar/Program.cs
@@ -2,19 +2,23 @@
 
 //Day 1
 
-// var elves = Day1.PopulateElvesList("../../../Day 1/elfs.txt");
-//
-// var mostValuableElf = Day1.FindMostValuedElf(elves!);
-// Console.WriteLine("Most valuable elf has " + mostValuableElf!.Calories + " calories");
-//
-// Console.WriteLine("Three most valuable elves have a caloriesum of: " +  Day1.ReturnWeightForThreeMostValuableElves(elves!));
+Console.WriteLine("-------------- DAY 1 ----------------");
+var elves = Day1.PopulateElvesList("../../../Day 1/elfs.txt");
 
+var mostValuableElf = Day1.FindMostValuedElf(elves!);
+Console.WriteLine($"Most valuable elf has {mostValuableElf} calories");
+
+Console.WriteLine($"Three most valuable elves have a caloriesum of: {Day1.ReturnWeightForThreeMostValuableElves(elves!)}");
+
+Console.WriteLine();
 //Day 2
+Console.WriteLine("-------------- DAY 2 ----------------");
+
 var matches = Day2.PopulateMatchesTask1("../../../Day 2/rockpaperscissors.txt");
 
 var totalPoints = matches.Sum(x => x.Point);
-Console.WriteLine(totalPoints);
+Console.WriteLine($"Task 1: {totalPoints}");
 
 var matches2 = Day2.PopulateMatchesTask2("../../../Day 2/rockpaperscissors.txt");
 var totalPoints2 = matches2.Sum(x => x.Point);
-Console.WriteLine(totalPoints2);
+Console.WriteLine($"Task 2: {totalPoints2}");
